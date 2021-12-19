88d4710b572c29deaff7e8c781effe9d7ee6a79a
KamilKhairullin <kamil.khayrullin.personal@yandex.ru>
2021-12-20 01:12:50 +0300

    working version

--- Distributed-systems-project/AppDelegate.swift
+++ Distributed-systems-project/AppDelegate.swift
@@ -3,18 +3,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        guard let window = window else { return false }
+        let vc = TyperacerViewController()
+        let navController = UINavigationController(rootViewController: vc)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
-
-
 }
 
